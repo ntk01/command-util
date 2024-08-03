f412a257d3762c571529b7f7197de3175cfc00c2
ntk01 <n.taku.law@gmail.com>
2024-08-03 13:58:00 +0900

    fix

--- cututil/__init__.py
+++ cututil/__init__.py
@@ -4,7 +4,9 @@ import csv
 def proc(ifilename: str, ofilename: str, delimiter: str, quotechar: str, fields: list[int]):
     with open(ofilename, 'w', newline='') as writefile:
         with open(ifilename, newline='') as readfile:
-            reader = csv.reader(readfile, delimiter, quotechar)
-            writer = csv.writer(writefile, delimiter, quotechar)
+            reader = csv.reader(
+                readfile, delimiter=delimiter, quotechar=quotechar)
+            writer = csv.writer(
+                writefile, delimiter=delimiter, quotechar=quotechar)
             for row in reader:
                 writer.writerow(list(map(lambda x: row[x], fields)))
